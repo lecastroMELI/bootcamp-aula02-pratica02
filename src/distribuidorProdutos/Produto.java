efd7c293c52ce7322a9d2aeba40a7f067dbde267
lecastroMELI <leticia.castro@mercadolivre.com>
2022-06-26 18:54:34 -0300

    Implementada a classe Produto

---
+++ src/distribuidorProdutos/Produto.java
@@ -0,0 +1,49 @@
+package distribuidorProdutos;
+
+/*Crie uma classe Produto que possua os seguintes atributos: String nome e
+    double preco, ela deve definir os
+    métodos getters e setters para seus atributos,
+    um construtor que recebe todos os seus atributos como parâmetro e
+    o método toString().
+
+    Crie o método calcular() para o qual vamos passar um
+    parâmetro do tipo int chamado quantidadeDeProdutos; este método tem que
+    multiplicar o preço pela quantidade de produtos passados.*/
+
+public class Produto {
+    String name;
+    double price;
+
+    Produto(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void  setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+            "name='" + name + '\'' +
+            ", price=" + price +
+            '}';
+    }
+
+    public double calc(int productsQuantity) {
+        return productsQuantity * price;
+    }
+}
